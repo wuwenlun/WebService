32c602625ae02eb522c831bf4fe000abfa35abf0
user <user>
2017-05-16 13:43:40 +0800

    修改端口，利用tcp/ip monitor进行监控调试

--- ws_server/src/com/wuwenlun/ws/simple/Server.java
+++ ws_server/src/com/wuwenlun/ws/simple/Server.java
@@ -9,9 +9,9 @@ public class Server {
 	
 	public static void main(String[] args) {
 		//同一个端口发布多个服务
-		String address = "http://127.0.0.1:12345/weather";
+		String address = "http://127.0.0.1:8080/weather";
 		Endpoint.publish(address, new WeatherImpl());
-		String address1 = "http://127.0.0.1:12345/idcard";
+		String address1 = "http://127.0.0.1:8080/idcard";
 		Endpoint.publish(address1, new IDCardImpl());
 	}
 
